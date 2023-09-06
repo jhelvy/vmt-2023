700882101e738af1e4edf65b3228f8ac0ca98485
John Helveston <john.helveston@gmail.com>
2023-09-06 06:20:59 -0400

    delete old quantiles

--- code/0-globals.R
+++ code/0-globals.R
@@ -3,7 +3,8 @@
 PATH_DB_RAW <- "/Volumes/ExtremeSSD/marketcheck/db/clean/db"
 
 # PATH_DB <- here::here('data', 'db.parquet') # Sample of original data
-PATH_DB <- "/Volumes/ExtremeSSD/marketcheck/db/miles.parquet"
+# PATH_DB <- "/Volumes/ExtremeSSD/marketcheck/db/miles.parquet"
+PATH_DB <- "/Users/jhelvy/Desktop/marketcheck/db/miles.parquet"
 
 # Min and max vehicle ages
 
